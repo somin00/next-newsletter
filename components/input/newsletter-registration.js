dccfbbf210c2d7fa2c91534ce06ff4e59e0d2168
somin <smhs12@naver.com>
2023-11-21 18:12:35 +0900

    feat: newsletter api 요청 기능 구현

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,25 +1,35 @@
-import classes from './newsletter-registration.module.css';
+import { useRef } from "react";
+import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
+  const emailRef = useRef();
+  const registrationHandler = async (event) => {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-  }
+    //newsletter api
+    const response = await fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: emailRef.current.value }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      emailRef.current.value = "";
+    }
+
+    console.log(data.email);
+  };
 
   return (
     <section className={classes.newsletter}>
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-          />
+          <input type="email" id="email" ref={emailRef} placeholder="Your email" aria-label="Your email" />
           <button>Register</button>
         </div>
       </form>
