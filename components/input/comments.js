233de0fc8da88ea33d97bf33f4517edc20c1ed3c
somin <smhs12@naver.com>
2023-11-21 21:43:46 +0900

    feat: 댓글 불러오기 및 작성 기능 구현

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,29 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
 
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
 
-  function toggleCommentsHandler() {
+  const toggleCommentsHandler = async () => {
     setShowComments((prevStatus) => !prevStatus);
-  }
+  };
 
-  function addCommentHandler(commentData) {
+  const addCommentHandler = async (commentData) => {
     // send data to API
-  }
+    const response = await fetch(`/api/comment/${eventId}`, {
+      method: "POST",
+      body: JSON.stringify(commentData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    return data;
+  };
+
+  useEffect(() => {
+    if (showComments) {
+      fetch(`/api/comment/${eventId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => setComments(data.comments));
+    }
+  }, [showComments]);
 
   return (
     <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
+      <button onClick={toggleCommentsHandler}>{showComments ? "Hide" : "Show"} Comments</button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList comments={comments} />}
     </section>
   );
 }
--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react';
-import classes from './new-comment.module.css';
+import { useRef, useState } from "react";
+import classes from "./new-comment.module.css";
 
 function NewComment(props) {
   const [isInvalid, setIsInvalid] = useState(false);
@@ -8,7 +8,7 @@ function NewComment(props) {
   const nameInputRef = useRef();
   const commentInputRef = useRef();
 
-  function sendCommentHandler(event) {
+  async function sendCommentHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
@@ -17,39 +17,45 @@ function NewComment(props) {
 
     if (
       !enteredEmail ||
-      enteredEmail.trim() === '' ||
-      !enteredEmail.includes('@') ||
+      enteredEmail.trim() === "" ||
+      !enteredEmail.includes("@") ||
       !enteredName ||
-      enteredName.trim() === '' ||
+      enteredName.trim() === "" ||
       !enteredComment ||
-      enteredComment.trim() === ''
+      enteredComment.trim() === ""
     ) {
       setIsInvalid(true);
       return;
     }
 
-    props.onAddComment({
+    const data = await props.onAddComment({
       email: enteredEmail,
       name: enteredName,
       text: enteredComment,
     });
+
+    if (data) {
+      emailInputRef.current.value = "";
+      nameInputRef.current.value = "";
+      commentInputRef.current.value = "";
+    }
   }
 
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={sendCommentHandler}>
       <div className={classes.row}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your email</label>
-          <input type='email' id='email' ref={emailInputRef} />
+          <label htmlFor="email">Your email</label>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='name'>Your name</label>
-          <input type='text' id='name' ref={nameInputRef} />
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" ref={nameInputRef} />
         </div>
       </div>
       <div className={classes.control}>
-        <label htmlFor='comment'>Your comment</label>
-        <textarea id='comment' rows='5' ref={commentInputRef}></textarea>
+        <label htmlFor="comment">Your comment</label>
+        <textarea id="comment" rows="5" ref={commentInputRef}></textarea>
       </div>
       {isInvalid && <p>Please enter a valid email address and comment!</p>}
       <button>Submit</button>
