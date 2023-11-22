ff01dca462ade57679434f7ac4826e88d21b4b26
somin <smhs12@naver.com>
2023-11-22 17:47:40 +0900

    refactor: 데이터베이스 관련 util 함수 사용

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,4 @@
-import { connectToDB } from "../../lib/db";
+import { connectToDB, insertDocument } from "../../lib/db";
 
 const DB_URL = process.env.NEXT_PUBLIC_MONGO_URL;
 
@@ -14,14 +14,25 @@ export default async function handler(req, res) {
     return;
   }
 
-  const client = await connectToDB();
-  const db = await client.db();
-  const result = await db.collection("emails").insertOne({ email: email });
+  let client = null;
+
+  try {
+    client = await connectToDB();
+  } catch (error) {
+    res.status(500).json({ message: "데이터베이스 연결에 실패했습니다." });
+    return;
+  }
+
+  let result = null;
+  try {
+    result = await insertDocument(client, "emails", { email: email });
+    res.status(201).json({
+      message: "사용자 등록 성공",
+      userEmail: email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "사용자 등록 실패" });
+  }
 
   client.close();
-
-  res.status(201).json({
-    message: "뉴스 레터를 구독했습니다.",
-    userEmail: email,
-  });
 }
