9d1143a63e1013e0b0e4dfebad1915ee820ed08d
somin <smhs12@naver.com>
2023-11-22 17:26:15 +0900

    feat: comment api 구현

--- pages/api/comment/[eventId].js
+++ pages/api/comment/[eventId].js
@@ -1,17 +1,10 @@
-export default function handler(req, res) {
+import { connectToDB } from "../../../lib/db";
+
+export default async function handler(req, res) {
   const { eventId } = req.query;
 
-  if (req.method === "GET") {
-    const DUMMY_DATA = [
-      { id: "e1", email: "test@email.com", name: "somin", text: "comment" },
-      { id: "e2", email: "test1@email.com", name: "somin1", text: "comment1" },
-    ];
-
-    res.status(200).json({
-      message: "댓글 요청 성공",
-      comments: DUMMY_DATA,
-    });
-  }
+  const client = await connectToDB();
+  const db = await client.db();
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -20,19 +13,35 @@ export default function handler(req, res) {
       res.status(422).json({
         message: "올바른 형식이 아닙니다.",
       });
+      client.close();
       return;
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
+
+    const result = await db.collection("comments").insertOne(newComment);
+
+    if (result) newComment._id = result.insertedId;
 
     res.status(201).json({
       message: "댓글 저장 완료",
       newComment,
+    });
+  }
+
+  if (req.method === "GET") {
+    const client = await connectToDB();
+    const db = await client.db();
+    const comments = await db.collection("comments").find({ eventId: eventId }).sort({ _id: -1 }).toArray();
+
+    res.status(200).json({
+      message: "댓글 요청 성공",
+      comments,
     });
   }
 }
