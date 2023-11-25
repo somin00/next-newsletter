6904d86e125184ba400f140fc6f5f1de3d022ca1
somin <smhs12@naver.com>
2023-11-25 21:58:30 +0900

    feat: context 이용하여 알림창 띄우기 기능 구현

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,11 +1,15 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import classes from "./newsletter-registration.module.css";
+import NotificationContext from "../../store/notification-context";
 
 function NewsletterRegistration() {
   const emailRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
+
   const registrationHandler = async (event) => {
     event.preventDefault();
 
+    notificationCtx.showNotification({ title: "전송중", message: "데이터를 전송중입니다.", status: "pending" });
     //newsletter api
     const response = await fetch("/api/newsletter", {
       method: "POST",
@@ -19,7 +23,17 @@ function NewsletterRegistration() {
 
     if (response.ok) {
       emailRef.current.value = "";
-      alert(`${data.userEmail} 사용자 등록 완료`);
+      notificationCtx.showNotification({
+        title: "전송 완료",
+        message: "데이터 전송을 완료했습니다..",
+        status: "success",
+      });
+    } else {
+      notificationCtx.showNotification({
+        title: "전송 실패",
+        message: "데이터 전송을 실패했습니다..",
+        status: "error",
+      });
     }
   };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,21 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
+import { NotificationProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationProvider>
   );
 }
 
